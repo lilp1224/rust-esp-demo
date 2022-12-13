6a31f0013029534ec83837e19647fb7492f2c033
lilpum <lilpum1224@gmail.com>
2022-12-13 22:41:09 +0800

    第一次提交

---
+++ src/led_and_servo/mod.rs
@@ -0,0 +1,31 @@
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::PinDriver;
+use esp_idf_hal::ledc::{config::{Resolution, TimerConfig}, LedcDriver, LedcTimerDriver};
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::prelude::FromValueType;
+
+/* Wokwi Project code 350684622891254355 */
+
+pub fn start() {
+    let peripherals = Peripherals::take().unwrap();
+    let mut led = PinDriver::input_output(peripherals.pins.gpio2).unwrap();
+    let button = PinDriver::input(peripherals.pins.gpio3).unwrap();
+
+    let config = TimerConfig::default().frequency(50.Hz()).resolution(Resolution::Bits14);
+
+    let timer = LedcTimerDriver::new(peripherals.ledc.timer1, &config).unwrap();
+    let mut channel = LedcDriver::new(peripherals.ledc.channel1,
+                                      timer, peripherals.pins.gpio10,
+                                      &config).unwrap();
+    loop {
+        if button.is_low() {
+            led.set_high().unwrap();
+            // Bits14  0度 374    180度 2038
+            channel.set_duty(2038).unwrap();
+            FreeRtos::delay_ms(4000);
+        } else {
+            led.set_low().unwrap();
+            channel.set_duty(374).unwrap();
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod led_and_servo;
+
+fn main() {
+    esp_idf_sys::link_patches();
+    println!("Hello, world! from rust ~");
+    /*led灯和舵机简单demo*/led_and_servo::start();
+}
